dd4b661d9b31739fd091215e29e8e93a406e031e
Raditz Farhan <raditzfarhan@gmail.com>
2025-07-31 12:51:37 +0800

    Update migration to be same as filament migration

--- database/migrations/2024_01_01_000001_create_excel_import_tables.php
+++ database/migrations/2024_01_01_000001_create_excel_import_tables.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -15,15 +14,14 @@ return new class extends Migration
         if (!Schema::hasTable('imports')) {
             Schema::create('imports', function (Blueprint $table) {
                 $table->id();
-                $table->string('file_name');
-                $table->string('file_path')->nullable();
-                $table->string('importer');
-                $table->integer('total_rows')->default(0);
-                $table->integer('processed_rows')->default(0);
-                $table->integer('imported_rows')->default(0);
-                $table->integer('failed_rows')->default(0);
                 $table->timestamp('completed_at')->nullable();
-                $table->foreignId('user_id')->nullable()->constrained()->nullOnDelete();
+                $table->string('file_name');
+                $table->string('file_path');
+                $table->string('importer');
+                $table->unsignedInteger('processed_rows')->default(0);
+                $table->unsignedInteger('total_rows');
+                $table->unsignedInteger('successful_rows')->default(0);
+                $table->foreignId('user_id')->constrained()->cascadeOnDelete();
                 $table->timestamps();
             });
         }
@@ -33,7 +31,7 @@ return new class extends Migration
             Schema::create('failed_import_rows', function (Blueprint $table) {
                 $table->id();
                 $table->json('data');
-                $table->foreignId('import_id')->constrained('imports')->cascadeOnDelete();
+                $table->foreignId('import_id')->constrained()->cascadeOnDelete();
                 $table->text('validation_error')->nullable();
                 $table->timestamps();
             });
